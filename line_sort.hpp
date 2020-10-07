39839021df10bbead4bf2af2fbdc9775f64edbc3
Nikolay Lanbin <kolyalan@yandex.ru>
2020-10-07 18:53:41 +0300

    Docs is also done

--- line_sort.hpp
+++ line_sort.hpp
@@ -12,6 +12,7 @@ struct Line {
 /// 
 /// @param text a big piece of text, in which line.pos will be looked for.
 /// @param line a struct describing which line to look for.
+///
 ///----------------------------------------------------------------------
 void print_line(const char *text, Line line);
 
@@ -23,6 +24,7 @@ void print_line(const char *text, Line line);
 /// @param text a big piece of text, other parameters refer to it.
 /// @param a    description of the first line to compare.
 /// @param b    description of the second line to compare.
+///
 ///----------------------------------------------------------------------
 bool forward_comparator(const char *text, const Line &a, const Line &b);
 
@@ -31,6 +33,7 @@ bool forward_comparator(const char *text, const Line &a, const Line &b);
 ///     symbol. Returns 1 if not.
 ///
 /// @param c    the byte to check
+///
 ///----------------------------------------------------------------------
 bool is_trailing_byte(char c);
 
@@ -41,8 +44,9 @@ bool is_trailing_byte(char c);
 ///     Ignores all non-letter symbols in system locale.
 ///
 /// @param text a big piece of text, other parameters refer to it.
-/// @param a    description of the first line to compare.
-/// @param b    description of the second line to compare.
+/// @param  a   description of the first line to compare.
+/// @param  b   description of the second line to compare.
+///
 ///----------------------------------------------------------------------
 bool backwards_comparator(const char *text, const Line &a, const Line &b);
 
@@ -57,6 +61,7 @@ bool backwards_comparator(const char *text, const Line &a, const Line &b);
 /// @param begin        iterator to the beginning of vector being sorted.
 /// @param end          iterator to the end of vector being sorted.
 /// @param cmp          comparator function. Must return 1 if the second argument is LESS than the third.
+///
 ///----------------------------------------------------------------------
 void merge_sort_internal(const char *text, 
         std::vector<Line>::iterator buf_begin, std::vector<Line>::iterator buf_end, 
@@ -73,6 +78,7 @@ void merge_sort_internal(const char *text,
 /// @param begin    iterator to the beginning of vector being sorted.
 /// @param end      iterator to the end of vector being sorted.
 /// @param cmp      comparator function. Must return 1 if the second argument is LESS than the third.
+///
 ///----------------------------------------------------------------------
 void merge_sort(const char *text, std::vector<Line>::iterator begin, std::vector<Line>::iterator end, 
         bool (*cmp) (const char *, const Line &, const Line &));
@@ -85,6 +91,7 @@ void merge_sort(const char *text, std::vector<Line>::iterator begin, std::vector
 /// @param b        the second line to compare
 /// @param result   expected result of comparison. 1 for LESS. 0 for MORE OR EQUAL.
 /// @param cmp      comparator function to test.
+///
 ///----------------------------------------------------------------------
 void test_comparator(char *text, const Line &a, const Line &b, bool result, bool (*cmp) (const char *, const Line &, const Line &));
 
@@ -95,6 +102,7 @@ void test_comparator(char *text, const Line &a, const Line &b, bool result, bool
 ///
 /// @param text the text to look for lines in.
 /// @param size size of the text in bytes.
+///
 ///----------------------------------------------------------------------
 std::vector<Line> find_lines(char *text, size_t size);
 
